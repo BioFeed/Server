11f9b44917b57d591e6ca748cf2220be425fd5fc
Sckathach <sckathach@hackademint.org>
2023-06-27 14:31:21 +0200

    Issue #1

--- app.py
+++ app.py
@@ -6,30 +6,44 @@ from modules.tokens import verify_token
 app = Flask(__name__)
 
 
+def check(r: dict) -> bool:
+    if ('token' not in r.keys()) or (not verify_token(r['token'])):
+        print('Connection refused: wrong token or no token')
+        return False
+    else:
+        print('Connection authorized')
+        return True
+
+
 @app.route('/', methods=['GET'])
-def index():
+def index() -> str:
     return 'Flask Server is running'
 
 
 @app.route('/store_data', methods=['POST'])
-def store_data():
+def store_data() -> str:
     new_data = request.json
-    if ('token' in new_data.keys()) and (verify_token(new_data['token'])):
+    if check(new_data):
         return add_data(new_data)
     else:
-        print('x> Connection refused: wrong token or no token')
         return 'Not authorized'
 
 
-@app.route('/command', methods=['GET'])
-def command():
-    cmd = request.args.get('cmd')
-    if cmd == 'save':
-        return save_data()
-    elif cmd == 'clear':
-        return clear_data()
+@app.route('/command', methods=['POST'])
+def command() -> str:
+    r = request.json
+    if check(r):
+        if 'command' in r.keys:
+            if r['command'] == 'save':
+                return save_data()
+            elif r['command'] == 'clear':
+                return clear_data()
+            else:
+                return 'Command unknown'
+        else:
+            return 'No command'
     else:
-        return 'Command unknown'
+        'Not authorized'
 
 
 app.run()
